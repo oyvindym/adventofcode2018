179b5c68b512b3a4eb107191e0ef317176f05f56
Oyvind Moldestad <moldestad@gmail.com>
2018-12-01 12:10:46 +0100

    First day completed

---
+++ 1/task.ts
@@ -0,0 +1,35 @@
+export const applyChanges = (changes: string, startFrequency = 0): number =>
+  changes
+    .split(' ')
+    .reduce(
+      (frequency: number, n: string): number => (frequency += parseInt(n)),
+      startFrequency
+    );
+
+export const findRepeatingFrequency = (
+  changes: string,
+  frequency = 0,
+  visitedFrequencies = [0],
+  found = false
+): number => {
+  if (found) {
+    return frequency;
+  }
+
+  changes
+    .split(' ')
+    .map(change => parseInt(change))
+    .forEach(change => {
+      if (!found) {
+        frequency += change;
+      }
+
+      if (visitedFrequencies.indexOf(frequency) !== -1) {
+        found = true;
+      } else {
+        visitedFrequencies.push(frequency);
+      }
+    });
+
+  return findRepeatingFrequency(changes, frequency, visitedFrequencies, found);
+};
